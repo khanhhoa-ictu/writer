3bb4c682c4ebc98be77a29826d3afd9c452422cf
khanh_hoa <your_email@youremail.com>
2020-10-13 13:46:32 +0700

    update index

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,6 @@ import {
 import Header from './Header';
 import Navigation from './Navigation';
 import Footer from './Footer';
-
 import routes from './../route-config';
 
 class App extends Component {
