0c2963a47bef8429323ccd3df2e45dfbb955908c
khanh_hoa <your_email@youremail.com>
2020-10-13 14:02:44 +0700

    update img

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -5,7 +5,7 @@ class BlogItem extends Component {
 		return (
 		    <div className="bg-faded p-4 my-4">
 			    <div className="card card-inverse">
-			        <img className="card-img img-fluid w-100" src="img/slide-1.jpg" alt="slide-1" />
+			        <img className="card-img img-fluid w-100" src="./../public.img/slide-1.jpg" alt="slide-1" />
 			        <div className="card-img-overlay bg-overlay">
 			            <h2 className="card-title text-shadow text-white text-uppercase mb-2 text-xs">XỐT GIA VỊ HOÀN CHỈNH BARONA SƯỜN XÀO CHUA NGỌT</h2>
 			            <h5 className="text-shadow text-white">March 1, 2017</h5>
@@ -21,7 +21,7 @@ class BlogItem extends Component {
 						<p className="lead card-text text-shadow text-white w-50 d-none d-lg-block text-xs">
 							Bước 4: Cho sườn ra đĩa, dùng với cơm nóng.
 						</p>
-			            <a href="http://localhost:3000/" className="btn btn-secondary">xem thêm..</a>
+			            <a href="#" className="btn btn-secondary">xem thêm..</a>
 			        </div>
 			    </div>
 			</div>
